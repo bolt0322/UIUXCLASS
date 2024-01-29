842d8b9c15492f362f22733e279f5c1455c2c633
bolt0322 <wls324jh@gmail.com>
2024-01-29 17:50:23 +0900

    240129

---
+++ 240129/flexproject02/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".menu-toggle-btn");
+const gnb = document.querySelector(".gnb");
+console.log(gnb);
+
+btn.addEventListener("click", () => {
+  gnb.classList.toggle("active");
+});
