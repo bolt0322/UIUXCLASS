e1d52b2017907cfdf533efeb79ed1086e5ae02dc
bolt0322 <wls324jh@gmail.com>
2024-02-14 18:31:16 +0900

    240214

--- myproject/script.js
+++ myproject/script.js
@@ -1,9 +1,10 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector(".header");
-  const sectionTravel = document.querySelector(".section-travel");
+  const sectionTravel = document.querySelector("#travel");
   const headerHeight = header.offsetHeight;
   if (window.scrollY >= sectionTravel.offsetTop - headerHeight) {
     header.style.height = "0";
+    header.style.overflow = "hidden";
   } else {
     header.style.height = "80px";
   }
